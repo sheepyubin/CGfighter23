50682ac0a4d43d9b77f2316c4dd29ea6dc08064f
PyoDev <eunpyo0621@gmail.com>
2023-11-04 22:45:22 +0900

    Lee

---
+++ SuperSlaves/Assets/Scenes/Lee_/ReadyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadyManager : MonoBehaviour
+{
+    public Vector2[] characterPos;      //캐릭터 선택창 포지션
+    public GameObject[] character;      //캐릭터 배열
+    float gap;                          //캐릭터별 간극
+
+
+    void Start()
+    {
+        for(int i =0; i < character.Length; i++)
+        {
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
